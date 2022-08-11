4c82a6dbfd8114b4c328e4146bd93e74b547e204
arnehogstad <arnehogstad@gmail.com>
2022-08-11 14:50:59 +0200

    Added logic for hot water
    title

--- src/components/tools/ekspansjonskar/Ekspansjonskar.js
+++ src/components/tools/ekspansjonskar/Ekspansjonskar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Inndata from './components/Inndata'
+import InndataVarme from './components/InndataVarme'
+import InndataTappevann from './components/InndataTappevann'
 import Beregning from './components/Beregning'
 import Produkter from './components/Produkter'
 import mockDatabase from "./components/mockDatabase.js"
@@ -10,8 +11,10 @@ import "../../../styles/ekspansjonskar/ekspansjonskar.css";
 export default function Ekspansjonskar(props){
   //redux bestemmer om vi skal vise app eller ikke
   const showTool = useSelector((state) => state.tool.visibleId)
-  //state over inndata
-  const [data, setData] = React.useState({
+  //state for om det beregnes for tappevann eller for varme
+  const [beregningsType, setBeregningsType] = React.useState("Varme")
+  //state over inndata for ekspansjonskar varmeanlegg
+  const [dataVarme, setDataVarme] = React.useState({
     ladetrykk: 1.5,
     sikkerhetsventil:3.5,
     vannvolum: 0,
@@ -19,6 +22,17 @@ export default function Ekspansjonskar(props){
     returtemp: 30,
     fluid: "Vann",
   })
+
+  //state over inndata for ekspansjonskar varmeanlegg
+  const [dataTappevann, setDataTappevann] = React.useState({
+    ladetrykk: 5.5,
+    nettvann: 6,
+    sikkerhetsventil:9,
+    vannvolum: 0,
+    temperatur: 65,
+    fluid: "Vann",
+  })
+
   const [expansion, setExpansion] = React.useState({
     ekspansjon: 0,
     nytteeffekt: 0,
@@ -30,8 +44,8 @@ export default function Ekspansjonskar(props){
   //endrer inndata
   function updateData(event){
     const {name, value} = event.target
-    setData(oldData => (
-        {...data,
+    setDataVarme(oldData => (
+        {...dataVarme,
         [name]:name === "fluid" ? value : parseFloat(value)
       }
     ))
@@ -39,42 +53,43 @@ export default function Ekspansjonskar(props){
 
   //endrer beregnet ekspansjon
   function updateExpansion(){
-    let tempObj=calcExpansion()
-    console.log(tempObj);
+    let tempObj=calcExpansionVarme()
     setExpansion(tempObj)
   }
 
 
 
   //mock database for testing calulation etc
-  const dataBase = mockDatabase.Ekspansjonsdata
+  const EkspansjonsData = mockDatabase.Ekspansjonsdata
+  const Ekspansjonskar = mockDatabase.Ekspansjonskar
+
   //elements for dropdown lists -> fluidtype
   const fluidListe = []
-  fluidListe.push(<option key="" value=""></option>)
-  for (const key in dataBase){
+
+  for (const key in EkspansjonsData){
     if (key !== "Temperatur"){
       fluidListe.push(<option key={key} value={key}>{key}</option>)
     }
   }
 
-  function calcExpansion(){
+  function calcExpansionVarme(){
     //beregner gj.sn temp
-    let meanTemp = (data.turtemp + data.returtemp)/2
+    let meanTemp = (dataVarme.turtemp + dataVarme.returtemp)/2
     //beregner sikkerhetsmargin, største verdi av 5% av volum og 3 liter
-    let safetyMargin = Math.max(0.005*data.vannvolum,3)
+    let safetyMargin = Math.max(0.005*dataVarme.vannvolum,3)
     //henter makstemperatur for valgte fluid
-    let tempMaxTempForFluid = dataBase.Temperatur[(dataBase[data.fluid].length-1)]
+    let tempMaxTempForFluid = EkspansjonsData.Temperatur[(EkspansjonsData[dataVarme.fluid].length-1)]
     //runder temp opp og ned for å få 2 punkter for interpoliering
     let meanTempHigh = Math.ceil(meanTemp/5)*5
     let meanTempLow = Math.floor(meanTemp/5)*5
     //henter relevant ekspansjonsmatrise basert på fluid
-    let tempFluidArr = dataBase[data.fluid]
+    let tempFluidArr = EkspansjonsData[dataVarme.fluid]
     //finner matriseposisjonene
-    let highIndex =  dataBase.Temperatur.findIndex((temp) => temp >= meanTempHigh)
-    let lowIndex =  dataBase.Temperatur.findIndex((temp) => temp >= meanTempLow)
+    let highIndex =  EkspansjonsData.Temperatur.findIndex((temp) => temp >= meanTempHigh)
+    let lowIndex =  EkspansjonsData.Temperatur.findIndex((temp) => temp >= meanTempLow)
     //beregner stor og liten ekspansjon
-    let tempExpHigh = Math.ceil(data.vannvolum*tempFluidArr[highIndex])
-    let tempExpLow = Math.ceil(data.vannvolum*tempFluidArr[lowIndex])
+    let tempExpHigh = Math.ceil(dataVarme.vannvolum*tempFluidArr[highIndex])
+    let tempExpLow = Math.ceil(dataVarme.vannvolum*tempFluidArr[lowIndex])
     //interpolerer for å finne faktisk ekspansjon
     let tempExp = 0
     if (meanTempLow === meanTempHigh){
@@ -83,7 +98,7 @@ export default function Ekspansjonskar(props){
       tempExp = (meanTemp-meanTempLow)/(meanTempHigh-meanTempLow)*(tempExpHigh-tempExpLow)+(tempExpLow)
     }
     //results for the object
-    let expansion = data.vannvolum === 0 ? 0 : Math.round((tempExp + safetyMargin)*10)/10
+    let expansion = dataVarme.vannvolum === 0 ? 0 : Math.round((tempExp + safetyMargin)*10)/10
     let usablePercentage = calcUsablePercentage()
     let minVolum = Math.ceil(expansion/usablePercentage)
 
@@ -99,10 +114,10 @@ export default function Ekspansjonskar(props){
   function calcUsablePercentage(){
     //statisk trykk
     let staticSafetyMargin = 0.2
-    let staticP = parseFloat(data.ladetrykk+staticSafetyMargin)
+    let staticP = parseFloat(dataVarme.ladetrykk+staticSafetyMargin)
     //blåsetrykk
     let valveSafetyMargin = 0.5
-    let safetyValveP = parseFloat(data.sikkerhetsventil-valveSafetyMargin)
+    let safetyValveP = parseFloat(dataVarme.sikkerhetsventil-valveSafetyMargin)
     //atmosfæreisk trykk
     let atmosP = 1
     //ekspansjonskarets nytteeffekt
@@ -115,24 +130,51 @@ export default function Ekspansjonskar(props){
     if(showResult === true){
       setShowResult(false)
     }
-  },[data])
+  },[dataVarme])
 
   return(
     <div className= {props.toolId === showTool ? "toolArea" : "hiddenTool"}>
       <div className="dataArea">
         <div className="toolInfo">
-          Test av render EkspansjonsKar ved bruk av redux
+          Finn passende ekspansjonskar ved å legge inn anleggsinformasjon<br></br>
+          Merk at det er bladibladi
         </div>
-        <Inndata
-          data={data}
-          updateData={updateData}
-          fluidListe={fluidListe}
-        />
+        <div className="ekspansjonskar-inndatalinje">
+          <div className="ekspansjonskar-inndataTittel">
+            Type ekspansjonskar
+          </div>
+          <div className="ekspansjonskar-inndataVerdi">
+            <select
+              name="sikkerhetsventil"
+              className="ekspansjonskar-select"
+              value={beregningsType}
+              onChange={(event) => setBeregningsType(event.target.value)}
+              >
+              <option key="varme" value="varme">Varme-/kjøleanlegg</option>
+              <option key="tappevan" value="tappevan">Tappevan</option>
+            </select>
+          </div>
+        </div>
+        <>
+        {beregningsType === "varme" ?
+          <InndataVarme
+            data={dataVarme}
+            updateData={updateData}
+            fluidListe={fluidListe}
+          />
+          :
+          <InndataTappevann
+            data={dataVarme}
+            updateData={updateData}
+            fluidListe={fluidListe}
+          />
+        }
+        </>
         <Beregning
           expansion={expansion}
         />
         <Produkter
-          data={data}
+          data={dataVarme}
           showResult = {showResult}
           setShowResult = {setShowResult}
         />

831a348d593f4b64d571b04b0c803cf407dca545
arnehogstad <arnehogstad@gmail.com>
2022-08-12 10:29:10 +0200

    Finished inndata-tappevann
    title

--- src/components/tools/ekspansjonskar/components/Beregning.js
+++ src/components/tools/ekspansjonskar/components/Beregning.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Beregning(props){
   const ekspansjon = props.expansion.ekspansjon === undefined ? 0 : props.expansion.ekspansjon
-  const nytteEffekt = props.expansion.nytteeffekt *100
+  const nytteEffekt = Math.round(parseFloat(props.expansion.nytteeffekt)*100)
   const minVolum = props.expansion.minimumVolum
   return(
     <div className="ekspansjonskar-beregning">

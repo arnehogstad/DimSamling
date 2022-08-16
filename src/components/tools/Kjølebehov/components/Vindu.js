78760ee4159b28407464e34eee623f5a1875a505
eaabk <ea@abkqviller.no>
2022-08-16 15:41:45 +0200

    Updated

--- src/components/tools/Kjølebehov/components/Vindu.js
+++ src/components/tools/Kjølebehov/components/Vindu.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { vindu_rettning, Avsjkerming } from "./data"
 
 
@@ -8,13 +8,13 @@ export default function InnData() {
             vinduArealet: 1,
             avskjerming: "Uten Avskjerming",
             vinduRettning: "Sør",
-            effekt:100
+            effekt: 100
         }
     )
 
 
     const [vindus, setVindus] = React.useState([])
-    const [vinduTable,setVinduTable]=React.useState([])
+    const [vinduTable, setVinduTable] = React.useState([])
 
 
     const avskjerming_type = Object.keys(Avsjkerming)
@@ -22,109 +22,108 @@ export default function InnData() {
 
     function handleChange(event) {
         const { name, value } = event.target
-       
-       
         setVinduData(prev => {
             return {
                 ...prev,
                 [name]: value,
-                }
+            }
         })
     }
 
-    function solEffect(vinduRettning,avskjerming, vinduArealet) {
+    function solEffect(vinduRettning, avskjerming, vinduArealet) {
      
-        let last =100
         if (vinduRettning === "Sør") {
-           last= (44+7*32)*vinduArealet*avskjerming 
-        }else if (vinduRettning === "Vest-Øst"){
-            last = (11)*vinduArealet*avskjerming*32
-        }else if(vinduRettning === "Nord"){
-            last= (6)*vinduArealet*avskjerming*32
+            return  523
+            // (44 + 7 * 32) * vinduArealet * avskjerming
+        } else if (vinduRettning === "Vest-Øst") {
+            return (11) * vinduArealet * avskjerming * 32
+        } else if (vinduRettning === "Nord") {
+            return (6) * vinduArealet * avskjerming * 32
         }
-        console.log(Avsjkerming[avskjerming])
-        setVinduData(prev => {
-             return {...prev,
-                effekt:last  }
-             })
-    }
-   
-    
-    function saveVindu() {
-        console.log(vinduData)
-       solEffect(vinduData.vinduRettning,Avsjkerming[vinduData.avskjerming],vinduData.vinduArealet)
-          
-       
-       setVindus(prev => [...prev, { vinduData }])
-       
-       setVinduTable(vindus.map( (item) =>(
-        <tr>
-        <td>{item.vinduData.vinduArealet}</td>
-        <td>{item.vinduData.avskjerming}</td>
-        <td>{item.vinduData.vinduRettning}</td>
-        <td>{item.vinduData.effekt}</td>
-        </tr>   
-       )))
                 
-}
+    }
 
 
- 
-return (
-    <div>
-        <form>
-
-            <label htmlFor="avskjerming">Avsjkerming:</label>
-            <select
-                id="avskjerming"
-                value={vinduData.avskjerming}
-                onChange={handleChange}
-                name="avskjerming"
-            >
-                {avskjerming_type.map((item) => (
-                    <option value={item}>{item}</option>
-                ))}
-            </select>
-
-            <label htmlFor="vinduRettning">Vindu Rettning:</label>
-            <select
-                id="vinduRettning"
-                value={vinduData.vinduRettning}
-                onChange={handleChange}
-                name="vinduRettning"
-            >
-                {vindu_rettning.map((item) => (
-                    <option value={item}>{item}</option>
-                ))}
-            </select>
-
-            <label>Vindu Arealet [m2]:
-                <input
-                    type="number"
-                    onChange={handleChange}
-                    name="vinduArealet"
-                    value={vinduData.vinduArealet}
-                /></label>
-
-
-        </form>
-        <button className="handlingsKnapp" onClick={saveVindu}>Lagre Vindu</button>
+    function saveVindu() {
 
         
+        setVinduData(prev => {
+            return {
+                ...prev,
+                effekt: solEffect(vinduData.vinduRettning, Avsjkerming[vinduData.avskjerming], vinduData.vinduArealet)
+            }
+        })
+        
+        setVindus(prev => [...prev, { vinduData }])
+
+        }
+
+   useEffect(()=>{ 
+        setVinduTable(vindus.map((item) => (
+        <tr>
+            <td>{item.vinduData.vinduArealet}</td>
+            <td>{item.vinduData.avskjerming}</td>
+            <td>{item.vinduData.vinduRettning}</td>
+            <td>{item.vinduData.effekt}</td>
+        </tr>
+    )))
+},[vinduData])
+
+    return (
+        <div>
+            <form>
+
+                <label htmlFor="avskjerming">Avsjkerming:</label>
+                <select
+                    id="avskjerming"
+                    value={vinduData.avskjerming}
+                    onChange={handleChange}
+                    name="avskjerming"
+                >
+                    {avskjerming_type.map((item) => (
+                        <option value={item}>{item}</option>
+                    ))}
+                </select>
+
+                <label htmlFor="vinduRettning">Vindu Rettning:</label>
+                <select
+                    id="vinduRettning"
+                    value={vinduData.vinduRettning}
+                    onChange={handleChange}
+                    name="vinduRettning"
+                >
+                    {vindu_rettning.map((item) => (
+                        <option value={item}>{item}</option>
+                    ))}
+                </select>
+
+                <label>Vindu Arealet [m2]:
+                    <input
+                        type="number"
+                        onChange={handleChange}
+                        name="vinduArealet"
+                        value={vinduData.vinduArealet}
+                    /></label>
 
 
-        <div className="table">
-            <table>
-                <tr>
-                    <th>Vindu Arealet [m2]</th>
-                    <th>Avskjerming</th>
-                    <th>Vindu Rettning</th>
-                    <th>Last [W]</th>
-                </tr>
-                {vinduTable}
-                
-            </table>
+            </form>
+            <button className="handlingsKnapp" onClick={saveVindu}>Lagre Vindu</button>
+
+
+
+
+            <div className="table">
+                <table>
+                    <tr>
+                        <th>Vindu Arealet [m2]</th>
+                        <th>Avskjerming</th>
+                        <th>Vindu Rettning</th>
+                        <th>Last [W]</th>
+                    </tr>
+                    {vinduTable}
+
+                </table>
+            </div>
         </div>
-    </div>
-)
+    )
 }

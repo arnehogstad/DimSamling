6fbc5979ac55c4d6383bd13924b62f19404f4367
eaabk <ea@abkqviller.no>
2022-09-06 10:07:16 +0200

    print function added

--- src/components/tools/Kjølebehov/Kjølebehov.js
+++ src/components/tools/Kjølebehov/Kjølebehov.js
@@ -6,6 +6,7 @@ import Ovrigelast from './components/Ovrigelast'
 import "../../../styles/kjølebehov/kjølebehov.css"
 import Oversikt from './components/Oversikt'
 
+
 export default function Kjølebehov(props) {
   const showTool = useSelector((state) => state.tool.visibleId)
 
@@ -14,29 +15,28 @@ export default function Kjølebehov(props) {
   const [lasts, setLasts] = useState([]);
   const last_data = (last) => { setLasts(last) }
 
-//To move data up a compnonet from a child component for the data in Vindus.js
-const [vindus, setVindus] = useState([]);
-const vindu_data = (vindu) => { setVindus(vindu) }  
+  //To move data up a compnonet from a child component for the data in Vindus.js
+  const [vindus, setVindus] = useState([]);
+  const vindu_data = (vindu) => { setVindus(vindu) }
 
-//To move data up a compnonet from a child component for the data in Øvrigelast.js
-const [ovriges, setOvriges] = useState([]);
-const ovrige_data = (ovrige) => { setOvriges(ovrige) }
+  //To move data up a compnonet from a child component for the data in Øvrigelast.js
+  const [ovriges, setOvriges] = useState([]);
+  const ovrige_data = (ovrige) => { setOvriges(ovrige) }
 
-console.log(lasts)
+  console.log(lasts)
   return (
     <div className={props.toolId === showTool ? "toolArea" : "hiddenTool"}>
       <div className="toolInfo">
 
-           
+        {lasts.length == 0 ? <InnData last_data={last_data} /> : null}
+        {lasts.length != 0 & vindus.length == 0 ? <Vindu vindu_data={vindu_data} /> : null}
+        {vindus.length != 0 & ovriges.length == 0 ? <Ovrigelast ovrige_data={ovrige_data} /> : null}
 
-         
-         {lasts.length ==0 ?   <InnData last_data={last_data} /> : null} 
-         {lasts.length !=0 & vindus.length==0   ?  <Vindu   vindu_data={vindu_data} />: null} 
-         {vindus.length !=0 & ovriges.length ==0  ?   <Ovrigelast ovrige_data={ovrige_data} /> : null} 
+
+        {ovriges.length != 0 ? <Oversikt lasts={lasts} ovrige={ovriges} vindus={vindus} /> : null}
         
-      
-     {ovriges.length !=0 ? <Oversikt lasts={lasts} ovrige={ovriges} vindus={vindus}/> : null}    
-    </div>
+        
+      </div>
     </div>
   )
 

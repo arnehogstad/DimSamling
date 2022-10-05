813ca6c98d3dbf796bdbfdd3de674bbd0338a2db
arnehogstad <arnehogstad@gmail.com>
2022-10-05 16:11:16 +0200

    Print floor separation
    title

--- src/components/tools/gulvvarme/components/printComponents/TableRow.js
+++ src/components/tools/gulvvarme/components/printComponents/TableRow.js
@@ -7,6 +7,7 @@ const styles = StyleSheet.create({
     alignItems: "left",
     borderColor: 'black',
     borderWidth: 0.5,
+    width: "100%",
   },
   rowHeader: {
     flexDirection: "row",
@@ -15,12 +16,13 @@ const styles = StyleSheet.create({
     borderWidth: 0,
     backgroundColor: 'gray',
     color: 'white',
+    width: "100%",
   },
   description: {
-    width: "30%",
+    width: "15%",
   },
   xyz: {
-    width: "20%",
+    width: "15%",
   },
   tableContainer: {
     flexDirection: "row",
@@ -31,15 +33,32 @@ const styles = StyleSheet.create({
 
 export default function TableRow(props){
 
-  console.log(props.unit)
 
-  const rows = props.unit.rooms.map((room,index) =>(
-    <View style={styles.row} key={room.id.toString()}>
-      <Text style={styles.description}>{room.name}</Text>
-      <Text style={styles.description}>{room.area}</Text>
-      <Text style={styles.xyz}>{room.cc}</Text>
-      <Text style={styles.xyz}>{room.circuits}</Text>
-    </View>
+  const sortedByFloor = props.unit.rooms.slice().sort((a,b)=>a.floor.localeCompare(b.floor))
+
+
+  const rows = sortedByFloor.map((room,index) =>(
+    <Fragment key={`${props.unit.unitId}${room.id}`}>
+    {index === 1 ?
+      <View style={styles.row} key={`${props.unit.unitId}${room.floor}2`}>
+        <Text style={styles.description}>{room.floor}</Text>
+      </View>
+      :
+      index > 1 && sortedByFloor[index-1].floor !== room.floor ?
+      <View style={styles.row} key={`${props.unit.unitId}${room.floor}2`}>
+        <Text style={styles.description}>{room.floor}</Text>
+      </View>
+      : null
+    }
+      <View style={styles.row} key={room.id.toString()}>
+        <Text style={styles.description}></Text>
+        <Text style={styles.description}></Text>
+        <Text style={styles.description}>{room.name}</Text>
+        <Text style={styles.description}>{room.area}</Text>
+        <Text style={styles.xyz}>{room.cc}</Text>
+        <Text style={styles.xyz}>{room.circuits}</Text>
+      </View>
+    </Fragment>
   ))
 
   return (
@@ -53,17 +72,29 @@ function TableHeader(props) {
   return(
     <View style={styles.tableContainer}>
       <View style={styles.rowHeader} key={`${props.unitId}1`}>
+        <Text style={styles.description}></Text>
+        <Text style={styles.description}></Text>
         <Text style={styles.description}>Rom</Text>
         <Text style={styles.description}>Areal</Text>
         <Text style={styles.xyz}>Røravstand</Text>
         <Text style={styles.xyz}>Antall kurser</Text>
       </View>
       <View style={styles.rowHeader}key={`${props.unitId}2`}>
+        <Text style={styles.description}></Text>
+        <Text style={styles.description}></Text>
         <Text style={styles.description}>[]</Text>
         <Text style={styles.description}>[m2]</Text>
         <Text style={styles.xyz}>[mm]</Text>
         <Text style={styles.xyz}>[stk]</Text>
       </View>
     </View>
+  )
+}
+
+function UnitHeadline(props) {
+
+
+  return (
+    null
   )
 }
